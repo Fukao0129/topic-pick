05e492a5a11e94738d960b0f2dee305a23237de5
DMS深尾 <t.fukao@dm-s.co.jp>
2026-05-27 16:22:25 +0900

    お気に入り操作にuseOptimisticを使う

--- src/features/summaries/components/favorite.tsx
+++ src/features/summaries/components/favorite.tsx
@@ -4,7 +4,7 @@ import { updateSummaryFavoriteAction } from "../actions/update";
 import { Icon } from "@/src/components/ui/icon";
 import { useSnackbar } from "@/src/components/ui/snackbar";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { startTransition, useOptimistic } from "react";
 
 /** サマリのお気に入りボタン
  * @param id サマリID
@@ -17,23 +17,26 @@ export const FavoriteSummary = ({
   id: number;
   favorite: boolean;
 }) => {
-  const [isFavorite, setIsFavorite] = useState(favorite);
+  const [optimisticFavorite, setOptimisticFavorite] = useOptimistic(favorite);
   const { showSnackbar } = useSnackbar();
 
-  const handleClick = async () => {
-    try {
-      await updateSummaryFavoriteAction(id, !isFavorite);
-      setIsFavorite(!isFavorite);
-    } catch {
-      showSnackbar("お気に入りの更新に失敗しました", "error");
-    }
+  /** お気に入りボタンのクリックハンドラ */
+  const handleClick = () => {
+    startTransition(async () => {
+      setOptimisticFavorite(!optimisticFavorite);
+      try {
+        await updateSummaryFavoriteAction(id, !optimisticFavorite);
+      } catch {
+        showSnackbar("お気に入りの更新に失敗しました", "error");
+      }
+    });
   };
 
   return (
     <Icon
       icon={faStar}
       clickable
-      color={isFavorite ? "warning" : "secondary"}
+      color={optimisticFavorite ? "warning" : "secondary"}
       size="small"
       onClick={handleClick}
     />
